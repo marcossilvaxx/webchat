4cece28b6c694e1722a67e2594b76cce6a5b15d8
Marcos Silva <vinicius_marcosmartins@hotmail.com>
2019-12-21 22:20:15 -0300

    Updating socket-client connection

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
